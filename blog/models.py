5204ac4bf1b5026568eaeacd78025d8e118ada16
Vanessa <vahnkoch@gmail.com>
2020-04-03 14:25:24 -0300

    add correção na sessão de comentários do blog

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
@@ -29,6 +33,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
